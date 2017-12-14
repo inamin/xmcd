a90c21e63ee26b4a46043ed1ba8988ac07a52de3
inamin <nobuhito.inami@gmail.com>
2017-12-14 15:24:53 +0900

    write README.md

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# XMCD analysis by python
+# XMCD analysis with python
+====
+Overview
+
+## Description
+
+## Demo
+
+## VS.
+
+[Larch: Data Analysis Tools for X-ray Spectroscopy](https://github.com/xraypy/xraylarch)
+
+## Requirement
+NumPy
+pandas
+SciPy
+matplotlib
+
+## Usage
+xmcd.py -c element.ini filelist.txt
+
+## Install
+
+## Contribution
+
+## License
+
+[MIT](https://opensource.org/licenses/mit-license.php)
+
+## Author
+
+[inamin](https://github.com/inamin)
+
--- xmcd.py
+++ xmcd.py
@@ -22,56 +22,6 @@ from fractions import Fraction
 
 from xas import Xas
 
-#class Xas:
-#    """ X-ray absorption spectrum """
-#    def __init__(self, filename, elem):
-#        self.elem = elem
-#        param = np.array([])  # type: float[param]
-#        cov = np.array([])  # type: float[cov]
-#        fit_preedge = np.array([])  # type: float[fit_preedge]
-# 
-#        print(filename)
-#        self.ene, self.i0, self.i1 = self._loaddat_pf(filename)
-#        self.mu = self.i1 / self.i0 # normalize by I0
-#        self.energy = elem.get_energy()
-#        self.mui = self._interpolate()
-#        self.e0 = self._find_e0()
-#        
-#
-#        self.fit_preedge = self._preedgefitting(elem.get_preedgemask())
-#
-#    def _loaddat_pf(self, filename):
-#        """
-#        load PF BL16A data
-#        """
-#        return np.loadtxt(filename, delimiter=" ", usecols=(0, 1, 2), unpack=True)
-#
-#    def _interpolate(self):
-#        f = interpolate.interp1d(self.ene, self.mu,
-#                                 kind="quadratic", fill_value="extrapolate")
-#        return f(self.energy)
-#
-#
-#    def _preedgefitting(self, mask):
-#        """ pre-edge fitting with linear function """
-#        return np.poly1d(np.polyfit(self.energy[mask], self.mui[mask], 2))(self.energy)
-#
-#    def _find_e0(self):
-#        """
-#        return E0
-#        """
-#        muidiff = np.diff(self.mui)
-#        e0index = np.argmax(muidiff)
-#        return self.energy[e0index]
-#
-#    def plot(self):
-#        plt.figure(figsize=(12, 6))
-#        plt.plot(self.ene, self.mu, "o")
-#        plt.plot(self.energy, self.mui, "r", label="quadratic")
-#        plt.legend()
-#        plt.show()
-
-
 class Xmcd:
     """ handle XMCD data """
 
